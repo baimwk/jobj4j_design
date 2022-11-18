612437cc46b426a09fcd7ad59863b714d7261efc
baimwk <blwkkz@gmail.com>
2022-11-18 14:21:47 +0300

    2.1.4. List 5. Очередь на двух стеках fix

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -4,12 +4,19 @@ package ru.job4j.collection;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
-    private final SimpleStack<T> in = new SimpleStack<>(); // обратный
-    private final SimpleStack<T> out = new SimpleStack<>();  //прямой
+    /**
+     * in - обратный
+     * out - прямой
+     */
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
     private int inCount;
     private int outCount;
 
-    public T poll() { // возвращать первое значение и удалять его
+    /**
+     * @return возвращать первое значение и удалять его
+     */
+    public T poll() {
         if (inCount == 0 && outCount == 0) {
             throw new NoSuchElementException();
         }
@@ -24,7 +31,10 @@ public class SimpleQueue<T> {
         return out.pop();
     }
 
-    public void push(T value) { //помещать значение в конец
+    /**
+     * @param value помещать значение в конец
+     */
+    public void push(T value) {
         in.push(value);
         inCount++;
     }
